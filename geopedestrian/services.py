6d95a8a0c78fe049c6eea1de3effa565af838753
gisfromscratch <gisfromscratch@live.de>
2024-06-10 18:20:22 +0200

    Updated the walk_to implementation

--- geopedestrian/services.py
+++ geopedestrian/services.py
@@ -3,6 +3,7 @@
 
 from georapid.client import GeoRapidClient
 import requests
+from typing import List
 
 
 
@@ -34,4 +35,37 @@ def solve_walking(client: GeoRapidClient, latitude: float, longitude: float):
     response = requests.request('GET', endpoint, headers=client.auth_headers, params=params)
     response.raise_for_status()
 
-    return response.json()    
+    return response.json()
+
+def walk_to(client: GeoRapidClient, latitude: float, longitude: float, category_id: int):
+    """
+    Walk to categorized places which are accessible by a pedestrian.
+
+    :param client: The client instance to use for this query.
+    :type client: :class:`georapid.client.GeoRapidClient`
+    :param latitude: The latitude representing the current pedestrian location.
+    :type latitude: float
+    :param longitude: The longitude representing the current pedestrian location.
+    :type longitude: float
+    :param category_id: The defined category.
+    :type category_id: int
+
+    :return: The accessible places with walking areas as GeoJSON.
+    """
+
+    if latitude < -90.0 or 90.0 < latitude:
+        raise ValueError(f'Invalid latitude value! {latitude} is not in the range of [-90.0, 90.0].')
+    
+    if longitude < -180.0 or 180.0 < longitude:
+        raise ValueError(f'Invalid longitude value! {longitude} is not in the range of [-180.0, 180.0].')
+    
+    endpoint = '{0}/walk_to'.format(client.url)
+    params = {
+        'lat': latitude,
+        'lon': longitude,
+        'category': category_id
+    }
+    response = requests.request('GET', endpoint, headers=client.auth_headers, params=params)
+    response.raise_for_status()
+
+    return response.json()  
